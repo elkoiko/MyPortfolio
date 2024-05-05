89521b5dae5715c527bba240dee0c6bdaf347f56
Thomas Couacault <me@thomaskoiko.com>
2024-05-05 23:52:56 +0200

    Add GitHub link

--- app/ui/components/socialLinks.tsx
+++ app/ui/components/socialLinks.tsx
@@ -2,6 +2,7 @@ import SocialLink from "./socialLink";
 
 const socialLinks: { title: string, linkText: string, url: string }[] = [
     { title: "Send me an email", linkText: "pro@thomaskoiko.com", url: "mailto:pro@thomaskoiko.com" },
+    { title: "GitHub", linkText: "@elkoiko", url: "https://github.com/elkoiko" },
     { title: "Linkedin", linkText: "in/thomas-couacault", url: "https://www.linkedin.com/in/thomas-couacault/" }
 ];
 
